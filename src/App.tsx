4f3b67a39d8a0c6ed5649c41ac2dfaa083846138
Warnigo Warfurious <abumain0012@gmail.com>
2025-02-15 09:18:42 +0500

    feat: added linting (eslint, prettier)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
+import reactLogo from "./assets/react.svg"
+
+import "./App.css"
+
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vite.dev" rel="noreferrer" target="_blank">
+          <img alt="Vite logo" className="logo" src={viteLogo} />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" rel="noreferrer" target="_blank">
+          <img alt="React logo" className="logo react" src={reactLogo} />
         </a>
       </div>
       <h1>Vite + React</h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react-swc"
+import path from "path"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-});
+  clearScreen: false,
+
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "src/app"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+      "@helpers": path.resolve(__dirname, "src/helpers"),
+      "@entities": path.resolve(__dirname, "src/entities"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@widget": path.resolve(__dirname, "src/widget"),
+    },
+  },
+})
